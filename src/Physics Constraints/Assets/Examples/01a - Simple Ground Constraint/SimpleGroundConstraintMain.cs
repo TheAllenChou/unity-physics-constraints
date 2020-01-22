a9efbaf3b9a24e7a690a18ac3df923c088c092ac
Allen Chou <minglun.chou@gmail.com>
2020-01-22 01:53:03 -0800

    Simple constraint implementations. Slides.

---
+++ src/Physics Constraints/Assets/Examples/01a - Simple Ground Constraint/SimpleGroundConstraintMain.cs
@@ -0,0 +1,44 @@
+﻿/******************************************************************************/
+/*
+  Project - Physics Constraints
+            https://github.com/TheAllenChou/unity-physics-constraints
+  
+  Author  - Ming-Lun "Allen" Chou
+  Web     - http://AllenChou.net
+  Twitter - @TheAllenChou
+*/
+/******************************************************************************/
+
+using UnityEngine;
+
+public class SimpleGroundConstraintMain : MonoBehaviour
+{
+  public float Gravity = -9.8f;
+  public float Beta = 0.2f;
+
+  public GameObject Ball;
+
+  private float vy = 0.0f;
+
+  private void FixedUpdate()
+  {
+    if (Ball == null)
+      return;
+
+    float dt = Time.deltaTime;
+    float y = Ball.transform.position.y;
+
+    vy += Gravity * dt;
+
+    if (y <= 0.0f)
+    {
+      vy = -(Beta / dt) * y;
+    }
+
+    y += vy * dt;
+
+    Vector3 pos = Ball.transform.position;
+    pos.y = y;
+    Ball.transform.position = pos;
+  }
+}
---
+++ src/Physics Constraints/Assets/Examples/01b - Simple Point Constraint/SimplePointConstraintMain.cs
@@ -0,0 +1,38 @@
+﻿/******************************************************************************/
+/*
+  Project - Physics Constraints
+            https://github.com/TheAllenChou/unity-physics-constraints
+  
+  Author  - Ming-Lun "Allen" Chou
+  Web     - http://AllenChou.net
+  Twitter - @TheAllenChou
+*/
+/******************************************************************************/
+
+using UnityEngine;
+
+public class SimplePointConstraintMain : MonoBehaviour
+{
+  public float Beta = 0.02f;
+
+  public GameObject Ball;
+  public GameObject P;
+
+  private Vector3 v = Vector3.zero;
+
+  private void Update()
+  {
+    if (Ball == null)
+      return;
+
+    if (P == null)
+      return;
+
+    float dt = Time.deltaTime;
+    Vector3 c = Ball.transform.position - P.transform.position;
+
+    v += (-Beta / dt) * c;
+    v *= 0.9f; // temp cheat
+    Ball.transform.position += v * dt;
+  }
+}
